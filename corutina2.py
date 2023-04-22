4cf26331acaa5052197d9163cebb1dc273eb6f15
Diego Cabrera <drcabreramendieta@gmail.com>
2023-04-22 08:48:05 -0500

    Se agrega inclusión de tareas al bucle de ejecución

---
+++ corutina2.py
@@ -0,0 +1,16 @@
+import asyncio
+from time import sleep
+
+async def corutina():
+    print("Hola mundo")
+    await asyncio.sleep(5)
+    # sleep(5)
+    print("Hola de nuevo")
+
+async def main():
+    tarea1 = asyncio.create_task(corutina())
+    tarea2 = asyncio.create_task(corutina())
+    await tarea1
+    await tarea2
+
+asyncio.run(main())
